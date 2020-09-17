3dde800dd7f4ce3793a4c459d54dfa433ccd8a14
heuzin <matheusqs.arq@gmail.com>
2020-09-17 18:03:16 -0400

    setting upp app.js JSX

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('running')
+
+// JSX = JavaScript XML
+const template = <p>This is JSX from app.js</p>
+const appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
