e76df2589758ea60b37fd364ff0a3a93f29364a3
heuzin <matheusqs.arq@gmail.com>
2020-09-30 21:23:47 -0400

    state counter example

--- src/app.js
+++ src/app.js
@@ -40,9 +40,17 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this)
+    }
+    handleRemoveAll() {
+        console.log(this.props.options)
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option}/>)
                 }
@@ -62,9 +70,21 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault()
+
+        const option = e.target.elements.option.value.trim()
+        if (option) {
+            alert(option)
+        }
+    }
     render() {
         return (
             <div>
+                <form onSubmit={this.handleAddOption}>
+                    <input type='text' name='option'/>
+                    <button>Add Option</button>
+                </form>
                 <p>Add Option</p>
             </div>
         )
